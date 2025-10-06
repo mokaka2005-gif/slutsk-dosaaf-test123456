4d794ac10c18bde53d515ca16c9ea263d2ac3b4c
mokaka2005-gif <mokaka.2005@gmail.com>
2025-10-06 15:04:58 +0300

    script.js

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const form = document.getElementById("form");
-  if (!form) return;
+  const form = document.getElementById("trainingForm");
   const status = document.getElementById("status");
+
+  if (!form) return;
 
   form.addEventListener("submit", async (e) => {
     e.preventDefault();
-    status.textContent = "Отправка...";
+    status.textContent = "⏳ Отправка...";
     const data = Object.fromEntries(new FormData(form).entries());
 
     try {
-      const res = await fetch("https://script.google.com/macros/s/AKfycbzNXKjdX-RDmb8Vf7EQVfTUTnIMfY5G5HslOrUXz-03atuUdOd80CcyjRqRIFO4gZFGug/exec", { // вставь ссылку на Google Script
+      const res = await fetch("https://script.google.com/macros/s/AKfycbzNXKjdX-RDmb8Vf7EQVfTUTnIMfY5G5HslOrUXz-03atuUdOd80CcyjRqRIFO4gZFGug/exec", {
         method: "POST",
-        body: JSON.stringify(data)
+        body: JSON.stringify(data),
+        headers: { "Content-Type": "application/json" },
       });
+
       const json = await res.json();
       if (json.ok) {
         status.textContent = "✅ Заявка успешно отправлена!";
